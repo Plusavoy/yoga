24cf410109c60a7a3264089c29b9a4132bd7f9ed
Plusavoy <vampir650972@mail.ru>
2020-03-24 14:59:40 +0300

    add Form

--- js/script.js
+++ js/script.js
@@ -82,14 +82,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
-        close = document.querySelector('.popup-close'),
-        tabsMore = document.querySelectorAll('.description-btn')[0];
-
-        tabsMore.addEventListener('click', function() {
-                overlay.style.display = 'block';
-                this.classList.add('more-splash'); 
-                document.body.style.overflow = 'hidden';
-        });
+        close = document.querySelector('.popup-close');
 
         more.addEventListener('click', function() {
             overlay.style.display = 'block';
@@ -123,6 +116,90 @@ window.addEventListener('DOMContentLoaded', function(){
         const item1 = new Options(500, 650, "red", 14, "center");
 
         item.createDiv();
+
+//Form
+
+        let message = {
+            loading: 'Загрузка...',
+            success: 'Спасибо! Скоро мы с Вами свяжемся!',
+            failure: 'Что-то пошло не так...'
+        };
+
+        let form = document.querySelector('.main-form'),
+            input = form.getElementsByClassName('input'),
+            statusMessage = document.createElement('div'),
+            formDown = document.getElementById('form');
+
+            statusMessage.classList.add('status');
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault(); //убирает стандартное поведение браузера(перезагрузку странницы при клике на отправку формы)
+            form.appendChild(statusMessage);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(form);
+
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('readystatechange', function() {
+                if(request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                }else if(request.readyState === 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.success;
+                }else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+
+            for(let i = 0; i < input.length; i++) {
+                input[i].value = ' ';
+            }
+
+        });
+
+        formDown.addEventListener('submit', function(event) {
+            event.preventDefault(); //убирает стандартное поведение браузера(перезагрузку странницы при клике на отправку формы)
+            form.appendChild(statusMessage);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(form);
+
+            let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('readystatechange', function() {
+                if(request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                }else if(request.readyState === 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.success;
+                }else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+
+            for(let i = 0; i < input.length; i++) {
+                input[i].value = ' ';
+            }
+
+        });
+            
 });
 
 
